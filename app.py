ddbf7eb24e3ca207688b89622e26ebefb49e118e
Parth Parmar <parthparmar4105@gmail.com>
2026-02-23 08:23:08 +0530

    Initial commit with Flask Azure Docker files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Flask running inside Docker on Azure 🚀"
+
+@app.route("/health")
+def health():
+    return {"status": "healthy"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
